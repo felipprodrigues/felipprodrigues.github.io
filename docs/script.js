e0d580a0af4a30616ba45326226e4e0d99bc9be9
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2021-05-22 19:09:30 -0300

    include color variables

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+// // COLOR THEME FUNCTION
+const btn = document.querySelector('.btn-toggle');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme'); 
+}); 
